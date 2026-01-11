fbdcc3ae23479e0c1e0cb662d6d4e06a699a116b
Rajnish Kumar <rajnishmaurya@Rajnishs-MacBook-Air.local>
2026-01-11 16:13:54 +0530

    config

--- server/utils/socket.js
+++ server/utils/socket.js
@@ -13,7 +13,7 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL ||"http://localhost:5173",
     },
   });
 
